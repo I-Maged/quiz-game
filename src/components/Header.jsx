093519030b1828349de4c6200dcc2a6644e83faa
I-Maged <magedhanafy@gmail.com>
2023-08-16 22:55:59 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div className='w-full bg-primary flex justify-center'>
+			<h1 className='text-5xl font-bold p-6'>Quiz Game!</h1>
+		</div>
+	);
+};
+
+export default Header;
